21a266f71efb3806763abebae478089173287c35
nevintrian <ne.vind99@gmail.com>
2020-06-25 13:12:09 +0700

    navigation bottom

--- inventoria/app/src/main/java/com/example/inventoria/MainActivity.java
+++ inventoria/app/src/main/java/com/example/inventoria/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.inventoria;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -8,32 +9,38 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.ui.AppBarConfiguration;
 
 import com.bumptech.glide.Glide;
 import com.example.inventoria.tools.SessionManager;
 
 import com.example.inventoria.tools.Url;
 import com.example.inventoria.ui.barang.BarangFragment;
+import com.example.inventoria.ui.cart.CartFragment;
 import com.example.inventoria.ui.gudang.GudangFragment;
 import com.example.inventoria.ui.home.HomeFragment;
 import com.example.inventoria.ui.keluar.KeluarFragment;
 import com.example.inventoria.ui.masuk.MasukFragment;
 import com.example.inventoria.ui.pelanggan.PelangganFragment;
 
+import com.example.inventoria.ui.pelanggan.search.SearchActivity;
 import com.example.inventoria.ui.profil.ProfilFragment;
 import com.example.inventoria.ui.sales.SalesFragment;
 import com.example.inventoria.ui.supplier.SupplierFragment;
 import com.example.inventoria.ui.user.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener{
+        implements NavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemSelectedListener{
 
     SessionManager sessionManager;
     @Override
@@ -42,7 +49,9 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view1);
+// beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
         sessionManager = new SessionManager(getApplicationContext());
         sessionManager.checkLogin();
 
@@ -80,6 +89,9 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.getMenu().getItem(0).setChecked(true);
     }
+
+
+
 
     @Override
     public void onBackPressed() {
@@ -148,6 +160,21 @@ public class MainActivity extends AppCompatActivity
                    .beginTransaction()
                    .replace(R.id.mainFrame, new KeluarFragment())
                    .commit();
+       } else if (id == R.id.navigation_home) {
+           getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.mainFrame, new HomeFragment())
+                   .commit();
+       } else if (id == R.id.navigation_notifications) {
+           getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.mainFrame, new ProfilFragment())
+                   .commit();
+       } else if (id == R.id.navigation_dashboard) {
+           getSupportFragmentManager()
+                   .beginTransaction()
+                   .replace(R.id.mainFrame, new CartFragment())
+                   .commit();
         } else if (id == R.id.nav_logout) {
             sessionManager.logoutUser();
         }
@@ -161,6 +188,9 @@ public class MainActivity extends AppCompatActivity
     public void onActivityReenter(int resultCode, Intent data) {
         super.onActivityReenter(resultCode, data);
     }
+
+
+
 
 
 
---
+++ inventoria/app/src/main/java/com/example/inventoria/ui/cart/CartFragment.java
@@ -0,0 +1,63 @@
+package com.example.inventoria.ui.cart;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.inventoria.R;
+import com.example.inventoria.model.User;
+import com.example.inventoria.network.response.UserResponse;
+import com.example.inventoria.tools.RecyclerItemClickListener;
+import com.example.inventoria.tools.SessionManager;
+import com.example.inventoria.tools.SimpleDividerItemDecoration;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.app.Activity.RESULT_OK;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CartFragment extends Fragment {
+
+    SessionManager session;
+
+
+    public CartFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View x = inflater.inflate(R.layout.fragment_cart, container, false);
+        ButterKnife.bind(this, x );
+        getActivity().setTitle("Keranjang");
+
+        session = new SessionManager(getActivity());
+
+        return x;
+    }
+
+
+
+}
